ba68769918d90850154f3b9de8e7c6a3da705124
Samir Maciel <samirmaciel.dev@gmail.com>
2025-11-22 23:13:00 -0300

    Updated version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sm.keepmarket"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
